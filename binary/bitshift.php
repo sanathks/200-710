9a30e310d7eb8b101c58df4784763b1f3745c833
Sanath Samarasinghe <sanath.samarasinghe@outlook.com>
2019-02-04 12:00:18 +0530

    bit shifting

--- binary/bitshift.php
+++ binary/bitshift.php
@@ -2,3 +2,21 @@
 /**
  * @author Sanath Samarasinghe <sanath@serrexlabs.com>
  */
+
+$x=8;
+$y=3;
+
+/*--------------------------------------------------------------
+    256 | 128 |  64  |  32  |  16  |  8  |  4  |  2  |  1  |
+----------------------------------------------------------------
+     0  |  0   |  0  |  0   |  0   |  1  |  0  |  0  |  0  | 8 = 1000 = $x
+----------------------------------------------------------------*/
+
+echo "Bit Shift Left\n";
+echo "x << y = ";
+echo $x << $y;  // So the bit is left that's mean is multiply by 2 by any given time here 8 * 2 * 2 * 2 = 64
+
+echo "\n";
+echo "Bit Shift Right\n";
+echo "x >> y = ";
+echo $x >> $y;  // So the bit is left that's mean is divided by 2 by any given time here 8 / 2 / 2 / 2 = 1
